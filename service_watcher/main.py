0263165ee32a4715d1eb98dc0b46d773b96894f8
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-12-14 19:56:56 +0700

    Wrap service related functions in Service class

--- service_watcher/main.py
+++ service_watcher/main.py
@@ -1,60 +1,60 @@
 import os
+from asyncio import sleep
+
 from telethon import TelegramClient, events
 
 
-def get_services_list():
-    with open("services.txt") as f:
-        # Parse comments and empty lines
-        services = [
-            line.strip()
-            for line in f.readlines()
-            if line.strip() and not line.strip().startswith("#")
-        ]
-    return services
+class ServiceWatcher:
+    def __init__(self) -> None:
+        pass
 
+    @staticmethod
+    def get_services_list():
+        with open("services.txt") as f:
+            # Parse comments and empty lines
+            services = [
+                line.strip()
+                for line in f.readlines()
+                if line.strip() and not line.strip().startswith("#")
+            ]
+        return services
 
-def is_service_running(service_name):
-    status = os.system(f"systemctl is-active --quiet {service_name}")
-    return status == 0
+    @staticmethod
+    def is_service_running(service_name):
+        status = os.system(f"systemctl is-active --quiet {service_name}")
+        return status == 0
 
-
-def main():
-
-    # To get API_ID and API_HASH, visit https://my.telegram.org
-    api_id = os.environ.get("TG_API_ID")
-    api_hash = os.environ.get("TG_API_HASH")
-
-    # To get BOT_TOKEN, visit https://t.me/BotFather
-    bot_token = os.environ.get("TG_BOT_SVCWATCH")
-
-    # To get USER_ID, visit https://t.me/userinfobot
-    user_id = os.environ.get("TG_USER_ID")
-
-    client = TelegramClient("svcwatcher", api_id, api_hash).start(bot_token=bot_token)
-
-    @client.on(events.NewMessage(pattern="/status"))
-    async def handler(event):
-
-        # Check if the message is from the user.
-        # If not, ignore it.
-        if event.sender_id != int(user_id):
-            return
-
-        services = get_services_list()
-
+    def get_services_status(self):
+        services = self.get_services_list()
         if not services:
-            message = "No services to check."
-            await event.respond(message)
-            return
+            return "No services to check."
 
         message = "**Service Status**\n"
 
         for service in services:
-            if is_service_running(service):
+            if self.is_service_running(service):
                 message += f"\n- `{service}` ::: __running__. "
             else:
                 message += f"\n- `{service}` ::: __not running__. "
 
+        return message
+
+
+def main():
+    api_id = os.environ.get("TG_API_ID")
+    api_hash = os.environ.get("TG_API_HASH")
+    bot_token = os.environ.get("TG_BOT_SVCWATCH")
+    user_id = os.environ.get("TG_USER_ID")
+
+    client = TelegramClient("svcwatcher", api_id, api_hash).start(bot_token=bot_token)
+    watcher = ServiceWatcher()
+
+    @client.on(events.NewMessage(pattern="/status"))
+    async def handler(event):
+        if event.sender_id != int(user_id):
+            return
+
+        message = watcher.get_services_status()
         await event.respond(message)
 
     client.start()
